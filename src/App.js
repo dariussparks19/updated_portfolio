381df139c29f1ff6cd86c79a3e278c6d8c5470d7
dariussparks19 <78409431+dariussparks19@users.noreply.github.com>
2024-01-25 16:22:32 -0800

    styled about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+//imported global styled
+import GlobalStyle from './components/GlobalStyle';
+
 import AboutMe from './pages/AboutMe';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutMe/>
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,98 @@
 import React, { Component } from "react";
 import ME from "../imgs/ME.jpg"
+//design
+import styled from "styled-components"
 
 const AboutSection = () => {
     return(
-        <div>
-            <div className="description">
+        <About>
+            <Description>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>Darius Sparks</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
                         <h2>Full <span>Stack</span> Engineer</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Junior software developer consistently improving technical acumen to jumpstart a career in <span> Front-End Development</span>. 
                     Ability to build web applications utilizing <span>React</span> with advanced skills in <span>HTML</span>, <span>CSS</span>, <span>JavaScript</span> and <span>Pyt</span><span>hon</span>. 
                     Enjoy working with a collaborative company that advocates in making a difference in the community and the world.
                 </p>
                 <button>Contact me</button>
-                <div className="image">
+            </Description>
+                <Image>
                     <img src={ME} alt="me"/>
-                </div>
-            </div>
-        </div>
+                </Image>
+        </About>
     )
 }
+
+//styled component
+const About = styled.div`
+    min-height: 85vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 7rem;
+    color: white;
+    @media (max-width: 1300px){
+        display: block;
+        padding: 2rem 2rem;
+        text-align: center;
+    }
+
+`
+
+const Description = styled.div`
+flex: 1;
+    z-index: 2;
+    padding-right: 2rem;
+    h2{
+        font-weight:lighter;
+    }
+    h4{
+        font-family: 'Inter', sans-serif;
+    }
+    span:nth-child(2){
+        color: #61DAFB
+    }
+    span:nth-child(3){
+        color: #E96228
+    }
+    span:nth-child(4){
+        color: #2965F1
+    }
+    span:nth-child(5){
+        color: #FEDC6F
+    }
+    span:nth-child(6){
+        color: #4D84A3
+    }
+    span:nth-child(7){
+        color: #FEDC6F
+    }
+    @media (max-width: 1300px){
+        padding: 0;
+        button{
+            margin: 2rem 0rem 5rem 0rem;
+        }
+    }
+`
+
+const Image = styled.div`
+    flex: 0.8;
+    overflow: hidden;
+    z-index: 2;
+    img{
+        width:100%;
+        height: 60vh;
+        object-fit: cover;
+    }
+`
+
+const Hide = styled.div`
+overflow: hidden;
+`
 
 export default AboutSection;
